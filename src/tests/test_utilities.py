17bcd0cf6040320afa03323b1a00a030dea10adf
Maruan Al-Shedivat <alshedivat@cs.cmu.edu>
2016-08-11 12:59:38 -0400

    Add tests for utilities

---
+++ src/tests/test_utilities.py
@@ -0,0 +1,18 @@
+import time
+
+from nig.utilities import elapsed_timer
+
+def test_elapsed_timer():
+    sleep_time = 0.01 # sec
+    with elapsed_timer() as elapsed:
+        time.sleep(sleep_time)
+        t_middle_1 = elapsed()
+        time.sleep(sleep_time)
+        t_middle_2 = elapsed()
+        time.sleep(sleep_time)
+    t_final_1 = elapsed()
+    time.sleep(sleep_time)
+    t_final_2 = elapsed()
+
+    assert t_middle_1 < t_middle_2 < t_final_1
+    assert t_final_1 == t_final_2
