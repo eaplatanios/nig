6c8abdcb0f70fe090b0072816bb21f5bc38a1762
Anthony Platanios <e.a.platanios@gmail.com>
2016-12-05 02:01:08 -0500

    Added an edge case to the delicious experiment.

--- src/experiment/delicious/simple_experiment.py
+++ src/experiment/delicious/simple_experiment.py
@@ -11,9 +11,9 @@ __author__ = 'eaplatanios'
 
 logger = logging.getLogger(__name__)
 
-architectures = [[512], [1024], [512, 512], [1024, 1024]]
+architectures = [[16], [512], [1024], [512, 256], [2048, 1024], [4096, 2048, 1024]]
 use_one_hot_encoding = True
-activation = tf.nn.relu
+activation = nig.leaky_relu(0.01)
 batch_size = 100
 labeled_batch_size = 100
 unlabeled_batch_size = 100
@@ -31,47 +31,51 @@ working_dir = os.path.join(os.getcwd(), 'working')
 checkpoint_file_prefix = 'ckpt'
 restore_sequentially = False
 save_trained = True
-optimizer = lambda: tf.train.AdamOptimizer(0.0001)
-gradients_processor = None  # norm_clipping(clip_norm=0.1)
+optimizer = lambda: nig.gradient_descent(
+    1e-1, decay_rate=0.99, learning_rate_summary=True)
+gradients_processor = None  # processors.norm_clipping(clip_norm=0.1)
 
 # optimizer = tf.contrib.opt.ScipyOptimizerInterface
 # optimizer_opts = {'options': {'maxiter': 10000}}
 
-with tf.device('/cpu:0'):
-    experiment = delicious.DeliciousExperiment(
-        architectures=architectures, activation=activation, batch_size=batch_size,
-        labeled_batch_size=labeled_batch_size,
-        unlabeled_batch_size=unlabeled_batch_size, max_iter=max_iter,
-        abs_loss_chg_tol=abs_loss_chg_tol, rel_loss_chg_tol=rel_loss_chg_tol,
-        loss_chg_iter_below_tol=loss_chg_iter_below_tol,
-        logging_frequency=logging_frequency, summary_frequency=summary_frequency,
-        checkpoint_frequency=checkpoint_frequency,
-        evaluation_frequency=evaluation_frequency,
-        variable_statistics_frequency=variable_statistics_frequency,
-        run_meta_data_frequency=run_meta_data_frequency, working_dir=working_dir,
-        checkpoint_file_prefix=checkpoint_file_prefix,
-        restore_sequentially=restore_sequentially, save_trained=save_trained,
-        optimizer=optimizer, gradients_processor=gradients_processor)
-    maj_00_consensus_learner = partial(
-        nig.ConsensusLearner, consensus_loss_weight=0.0, consensus_method='MAJ')
-    maj_0_consensus_learner = partial(
-        nig.ConsensusLearner, consensus_loss_weight=1e0, consensus_method='MAJ')
-    maj_1_consensus_learner = partial(
-        nig.ConsensusLearner, consensus_loss_weight=1e1, consensus_method='MAJ')
-    maj_2_consensus_learner = partial(
-        nig.ConsensusLearner, consensus_loss_weight=1e2, consensus_method='MAJ')
-    maj_3_consensus_learner = partial(
-        nig.ConsensusLearner, consensus_loss_weight=1e3, consensus_method='MAJ')
+# with tf.device('/gpu:0'):
+experiment = delicious.DeliciousExperiment(
+    architectures=architectures, activation=activation, batch_size=batch_size,
+    labeled_batch_size=labeled_batch_size,
+    unlabeled_batch_size=unlabeled_batch_size, max_iter=max_iter,
+    abs_loss_chg_tol=abs_loss_chg_tol, rel_loss_chg_tol=rel_loss_chg_tol,
+    loss_chg_iter_below_tol=loss_chg_iter_below_tol,
+    logging_frequency=logging_frequency, summary_frequency=summary_frequency,
+    checkpoint_frequency=checkpoint_frequency,
+    evaluation_frequency=evaluation_frequency,
+    variable_statistics_frequency=variable_statistics_frequency,
+    run_meta_data_frequency=run_meta_data_frequency, working_dir=working_dir,
+    checkpoint_file_prefix=checkpoint_file_prefix,
+    restore_sequentially=restore_sequentially, save_trained=save_trained,
+    optimizer=optimizer, gradients_processor=gradients_processor)
+maj_00_consensus_learner = partial(
+    nig.ConsensusLearner, consensus_loss_weight=0.0, consensus_method='MAJ')
+maj_0_consensus_learner = partial(
+    nig.ConsensusLearner, consensus_loss_weight=1e0, consensus_method='MAJ')
+maj_1_consensus_learner = partial(
+    nig.ConsensusLearner, consensus_loss_weight=1e1, consensus_method='MAJ')
+maj_2_consensus_learner = partial(
+    nig.ConsensusLearner, consensus_loss_weight=1e2, consensus_method='MAJ')
+maj_3_consensus_learner = partial(
+    nig.ConsensusLearner, consensus_loss_weight=1e3, consensus_method='MAJ')
+maj_10_consensus_learner = partial(
+    nig.ConsensusLearner, consensus_loss_weight=1e10, consensus_method='MAJ')
 
-    learners = {'Majority-0.0': maj_00_consensus_learner,
-                'Majority-1.0': maj_0_consensus_learner,
-                'Majority-10.0': maj_1_consensus_learner,
-                'Majority-100.0': maj_2_consensus_learner,
-                'Majority-1000.0': maj_3_consensus_learner}
-    experiment.run(learners, show_plots=False, plots_folder=working_dir)
+learners = {'Majority-0.0': maj_00_consensus_learner,
+            'Majority-1.0': maj_0_consensus_learner,
+            'Majority-10.0': maj_1_consensus_learner,
+            'Majority-100.0': maj_2_consensus_learner,
+            'Majority-1000.0': maj_3_consensus_learner,
+            'Majority-1e10': maj_10_consensus_learner}
+experiment.run(learners, show_plots=False, plots_folder=working_dir)
 
-    # test_predictions = learner.predict(
-    #     _get_iterator(test_data, False), ckpt=False, working_dir=working_dir,
-    #     ckpt_file_prefix=checkpoint_file_prefix)
-    # test_truth = test_data[:, -1]
-    # logger.info(np.mean(test_predictions == test_truth))
+# test_predictions = learner.predict(
+#     _get_iterator(test_data, False), ckpt=False, working_dir=working_dir,
+#     ckpt_file_prefix=checkpoint_file_prefix)
+# test_truth = test_data[:, -1]
+# logger.info(np.mean(test_predictions == test_truth))
